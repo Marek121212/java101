1e4f305bc5da07bf8b87bcc827584885a9ef7e9f
Marek121212 <marek.coronic@centrum.sk>
2023-06-21 09:49:20 +0200

    Aktualizacia reaktora 2

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -8,14 +8,24 @@ public class Reactor extends AbstractActor {
     private boolean state; // nevidi ich ani materska trieda
     private int damage;
     private Animation normalAnimation;
+    private Animation normalAnimationHot;
 
     public Reactor() { // konstruktor nic nevracia preto ani void nepise
         this.temperature = 0;
         this.state = false;
         this.damage = 0;
-        this.normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG); //prehrava obrazky 1,2,3,4,3,2,1;
+        this.normalAnimation = new Animation("sprites/reactor_on.png",
+            80, 80, 0.1f,
+            Animation.PlayMode.LOOP_PINGPONG); //prehrava obrazky 1,2,3,4,3,2,1;
+
+
+        this.normalAnimationHot = new Animation("sprites/reactor_hot.png",
+            80,80,0.1f,
+            Animation.PlayMode.LOOP_PINGPONG);
 
         setAnimation(this.normalAnimation);
+        //nastavujem len normalAnimation / ostatne nie lebo ako sa ma zobrazit prvy obrazok
+        //ak by som nastavil aj this.normalAnimationHot videl by som hned horuci obrazok...
     }
 
     public int getTemperatur() {
@@ -42,15 +52,11 @@ public class Reactor extends AbstractActor {
                 Animation.PlayMode.LOOP_PINGPONG
             );
             setAnimation(animation);
-
+            // ak by som nechcel vytvarat novu triedu ako v podnmienke pred tak definujem
+            // privat Animation normalAnimationHot a importujem don obrazok...
             // if (4000 <= temperature < 6000), then show hot reactor
         } else if (this.temperature >= 4000) {
-            Animation animation = new Animation(
-                "sprites/reactor_hot.png",
-                80, 80, 0.1f,
-                Animation.PlayMode.LOOP_PINGPONG
-            );
-            setAnimation(animation);
+            setAnimation(this.normalAnimationHot);
 
             // otherwise show normal reactor
         } else {
