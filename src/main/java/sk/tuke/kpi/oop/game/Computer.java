51be4ef633f94b5d071c9a189b1189e70dc92336
Marek121212 <marek.coronic@centrum.sk>
2023-06-22 08:14:17 +0200

    pridanie computeru

---
+++ src/main/java/sk/tuke/kpi/oop/game/Computer.java
@@ -0,0 +1,22 @@
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Computer extends AbstractActor {
+    public Computer() {
+        Animation animation = new Animation("sprites/computer.png", 80, 48,
+            0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        setAnimation(animation);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+
+    }
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+}
+
