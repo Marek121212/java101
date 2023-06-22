4adad51d1d0f9a255ef4ef22f76c230a5be2d205
Marek121212 <marek.coronic@centrum.sk>
2023-06-22 10:54:55 +0200

    vytvorenie ovladaca

---
+++ src/main/java/sk/tuke/kpi/oop/game/Controller.java
@@ -0,0 +1,11 @@
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Controller extends AbstractActor {
+    public Controller() {
+        Animation animation = new Animation("sprites/switch.png");
+        setAnimation(animation);
+    }
+}
