883702bf2abf05b2d33aa8eaec5be12dbd8c0f3e
Marek121212 <marek.coronic@centrum.sk>
2023-06-22 11:01:46 +0200

    prepojenie ovladaca a reactora

--- src/main/java/sk/tuke/kpi/oop/game/Controller.java
+++ src/main/java/sk/tuke/kpi/oop/game/Controller.java
@@ -4,7 +4,14 @@ import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Controller extends AbstractActor {
-    public Controller() {
+
+    private Reactor reactor;
+
+
+    public Controller(Reactor reactor) {
+        // reactor ktory je za == sme preradili do premennej ktora je private
+        // aby sme povedali ze dany controller ovlada dany reactor...
+        this.reactor = reactor;
         Animation animation = new Animation("sprites/switch.png");
         setAnimation(animation);
     }
