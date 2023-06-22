74c40b241e5f08b413e08c08b7d13558d1009fb9
Marek121212 <marek.coronic@centrum.sk>
2023-06-22 08:27:06 +0200

    akutalizacia computeru

--- src/main/java/sk/tuke/kpi/oop/game/Computer.java
+++ src/main/java/sk/tuke/kpi/oop/game/Computer.java
@@ -12,11 +12,16 @@ public class Computer extends AbstractActor {
 
     public int add(int a, int b) {
         return a + b;
+    }
 
+    public float add(float a, float b) {
+        return a + b;
     }
     public int sub(int a, int b){
         return a - b;
     }
-
+    public float sub(float a, float b) {
+        return a - b;
+    }
 }
 
